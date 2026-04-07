61ce6d8c660294e9f09da063f57424586aafb68c
Ed <ed.gie0696@gmail.com>
2026-04-07 15:18:55 +1200

    add keyboard support

--- calc.js
+++ calc.js
@@ -1,10 +1,19 @@
 "use strict";
 
-const display = document.querySelector(".display");
+const display = document.querySelector(".display p");
 const buttons = document.querySelector(".buttons");
-const controls = document.querySelector(".controls");
-const numbers = document.querySelector(".numbers");
-const operators = document.querySelectorAll(".operator")
+const controls = document.querySelectorAll(".controls button");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const numbers = document.querySelectorAll(".numbers button");
+
+function consoleLog() {
+    console.log(`firstNumber: ${firstNumber}`);
+    console.log(`secondNumber: ${secondNumber}`);
+    console.log(`op: ${op}`);
+    console.log(`readyForSecondNumber: ${readyForSecondNumber}`);
+    console.log(`resultDisplayed: ${resultDisplayed}`);
+}
 
 function add(num1, num2) {
     return num1 + num2;
@@ -58,10 +67,12 @@ buttons.addEventListener("click", (e) => {
             op = "";
             readyForSecondNumber = false;
             resultDisplayed = false;
+            display.style.fontSize = "";
         }
         if (readyForSecondNumber === false) {//input firstNumber
             display.textContent += e.target.textContent;
             firstNumber = display.textContent;
+            consoleLog();
         }
         else {//input secondNumber
             if (secondNumber === null) {
@@ -69,6 +80,8 @@ buttons.addEventListener("click", (e) => {
             }
             display.textContent += e.target.textContent;
             secondNumber = display.textContent;
+
+            consoleLog();
         }
     }
     else if (e.target.textContent === "AC") {
@@ -79,12 +92,18 @@ buttons.addEventListener("click", (e) => {
         readyForSecondNumber = false;
         operators.forEach(btn => btn.style.border = "");
         display.style.fontSize = "";
+
+        consoleLog();
     }
     else if (e.target.textContent === "⌫") {
         display.textContent = display.textContent.slice(0, -1);
     }
-    else if (e.target.classList.contains("operator")) {//input operator
-        if (op !== "") {
+    else if (e.target.classList.contains("operator") && e.target.textContent !== "=") {//input operator
+        if (op !== "") {// consecutive operator
+            if (firstNumber === "You can't divide by 0 idiot!") {
+                firstNumber = null;
+                display.style.fontSize = "";
+            }
             operators.forEach(btn => btn.style.border = "");
             display.textContent = operate(Number(firstNumber), Number(secondNumber), op);
             firstNumber = display.textContent;
@@ -93,6 +112,8 @@ buttons.addEventListener("click", (e) => {
         op = e.target.textContent;
         readyForSecondNumber = true;
         e.target.style.border = "4px solid green";
+
+        consoleLog();
     }
     else if (e.target.textContent === "=") {
         display.textContent = operate(Number(firstNumber), Number(secondNumber), op);
@@ -100,5 +121,37 @@ buttons.addEventListener("click", (e) => {
         secondNumber = null;
         operators.forEach(btn => btn.style.border = "");
         resultDisplayed = true;
+        consoleLog()
+    }
+})
+
+// keyboard support
+document.addEventListener("keydown", (e) => {
+    console.log("keydown fired", e.key);
+    const opKeyMap = {
+        "/": "÷",
+        "*": "×",
+        "+": "+",
+        "-": "-",
+        "=": "=",
+    };
+    const controlsKeyMap = {
+        "Backspace": "⌫",
+        "Escape": "AC"
+    }
+    if (!isNaN(e.key) || e.key === ".") {// numbers
+        const button = Array.from(numbers).find(btn => btn.textContent === e.key);
+        if (button) button.click();
+    }
+    else if (Array.from(operators).find(btn => btn.textContent === opKeyMap[e.key])) {// operators
+        const button = Array.from(operators).find(btn => btn.textContent === opKeyMap[e.key]);
+        if (button) button.click();
+    }
+    else if (Array.from(controls).find(btn => btn.textContent === controlsKeyMap[e.key])) {// backspace and AC
+        const button = Array.from(controls).find(btn => btn.textContent === controlsKeyMap[e.key]);
+        if (button) button.click();
+    }
+    else if (e.key === "Enter") {// =
+        equals.click();
     }
 })
